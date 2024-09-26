ed182b5ae31bc22527ae1d95d382f38bf84736d0
Pedro Soto Soto <dex.pgsoto@gmail.com>
2024-09-26 19:40:39 +0100

    feat: add sign-in form

--- src/app/auth/features/sign-in/sign-in.component.ts
+++ src/app/auth/features/sign-in/sign-in.component.ts
@@ -5,8 +5,6 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './sign-in.component.html',
-  styles: ``
+  styles: ``,
 })
-export class SignInComponent {
-
-}
+export default class SignInComponent {}
